dfacea220f9f6b992ac1c0495c7175a1121c52f8
Gregory Vincic <g@7de.se>
2022-09-25 06:16:17 +0200

    Add shortcuts r and a

--- webui/assets/enhance.js
+++ webui/assets/enhance.js
@@ -1,6 +1,15 @@
 /* todo */
 document.addEventListener('keydown', function(event) {
     console.log(event.key, event.keyCode);
+    switch (event.keyCode) {
+    case 82:
+	window.scrollTo(0, 0); 
+	break;
+    case 65:
+	document.getElementById("lastInsertButton").click();
+	break;
+    }
+	
 });
 /*
 r 82 
