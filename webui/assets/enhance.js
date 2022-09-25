c535f3fb70c0d1c17509505f10928824a5e11a33
Gregory Vincic <g@7de.se>
2022-09-25 06:37:43 +0200

    Modify shortcuts

--- webui/assets/enhance.js
+++ webui/assets/enhance.js
@@ -1,20 +1,42 @@
-/* todo */
 document.addEventListener('keydown', function(event) {
     console.log(event.key, event.keyCode);
     switch (event.keyCode) {
-    case 82:
-	window.scrollTo(0, 0); 
+    case 68: // d
+	window.scrollTo(0, document.body.scrollHeight);
 	break;
-    case 65:
-	document.getElementById("lastInsertButton").click();
+	
+    case 69: // e
+	document.getElementById("nav1").click();
+	break;
+
+    case 70: // f
+	window.scrollTo(0, 0);
+	break;
+	
+    case 82: // r
+	document.getElementById("nav2").click();	
+	break;
+	
+    case 65: // a
+	var element = document.getElementById("lastInsertButton");
+
+	if(typeof(element) != 'undefined' && element != null){
+            document.getElementById("lastInsertButton").click();
+	} 
 	break;
     }
-	
 });
-/*
-r 82 
-e 69 
-s 83 
-d 68 
-f 70
-*/
+
+function disableKeydownOnInputs() {
+    document.querySelectorAll('input').forEach(element => {
+	element.addEventListener('keydown', e => {
+	    e.stopPropagation();
+	});
+    });
+
+    document.querySelectorAll('textarea').forEach(element => {
+	element.addEventListener('keydown', e => {
+	    e.stopPropagation();
+	});
+    });
+}
